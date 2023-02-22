64b78b72d35a587656e32883a6884069b100739e
priyarakkan <priyadharsinikdp@gmail.com>
2023-02-22 12:12:48 -0800

    update js files

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,8 +1,4 @@
-/* Usage: Use simple conditional statements
 
- Then, display only the larger of the two 
-within the browser window.
-Don’t forget to handle the fact that the two could be equal.*/
 let number1 = prompt('Enter the first integer: ');
 let number2 = prompt('Enter the second integer: ');
 if (number1 > number2) {
--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,12 +1,4 @@
-/*Create an application that prompts the user for a language code 
-("es", "de", "en", and "fr"). If they enter one of the four codes, 
-display the translated version of "Hello World" for the given 
-language within the console window. If the user enters 
-anything but those four language codes, 
-the application should default to English. 
-The result should be (for example):
 
-Hello World translated in French is: Bonjour le monde */
 let lang = prompt('Enter your language code(en, de, en & fr): ');
 if (lang == 'es') {
     console.log('Hello World translated in Spanish is: Hola Mundo');
